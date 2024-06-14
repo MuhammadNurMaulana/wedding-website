fb181a4c1e9b25aec92192b5dcfeee829a2a9631
MuhammadNurMaulana <muhammadnrmaulana@gmail.com>
2024-06-14 08:14:25 +0700

    cancel redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "https://qrfy.io/8st3rF4LJk",
-        destination: "https://wedding-website.vercel.app/",
-        permanent: true,
-      },
-    ]
-  },
-}
+const nextConfig = {}
 
 export default nextConfig
